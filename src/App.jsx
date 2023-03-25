40d262324c10456ae53f867c081f386052a155dd
Ian Cenry <iancenry@gmail.com>
2023-03-25 23:41:10 +0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import {AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu} from './container'
 import {Navbar} from './components'
+import {AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu} from './container'
 
 function App() {
 
   return (
     <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
           
     </div>
   )
