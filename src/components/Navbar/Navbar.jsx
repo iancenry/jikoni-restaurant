eb1fce68c5c5fee86c7b3ec265868acc831953ab
Ian Cenry <iancenry@gmail.com>
2023-03-26 23:26:17 +0300

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,6 +16,21 @@ const Navbar = () => (
       <li className="p__opensans"><a href="#awards">Awards</a></li>
       <li className="p__opensans"><a href="#contact">Contact</a></li>
     </ul>
+    <div className="app__navbar-login">
+      <a href="#login" className='p__opensans'>Log In / Register</a>
+      <div />
+      <a href="/" className='p__opensans'>Book a Table</a>
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color='#fff' fontSize={27}  onClick={() => {}} />
+      <ul className="app__navbar-smallscreen-links">
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#Menu">Menu</a></li>
+        <li className="p__opensans"><a href="#awards">Awards</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
   </nav>
 );
 
