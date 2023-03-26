aa152617ebd1cf98a9110b4e3d1f898634940910
Ian Cenry <iancenry@gmail.com>
2023-03-27 00:23:44 +0300

    change Navbar to rafce

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,10 @@ import {MdOutlineRestaurantMenu} from 'react-icons/md'
 import images from '../../constants/images'
 import './Navbar.css';
 
-const Navbar = () => (
+const Navbar = () => {
+  
+
+  return (
   <nav className='app__navbar'>
     <div className="app__navbar-logo">
       <img src={images.gericht} alt="app logo " />
@@ -36,6 +39,8 @@ const Navbar = () => (
       </div>
     </div>
   </nav>
-);
+)
+};
+
 
 export default Navbar;
