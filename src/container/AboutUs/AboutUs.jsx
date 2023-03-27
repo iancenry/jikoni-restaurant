6e69bcafc95d26e0f111177350dc96cbea3f1870
Ian Cenry <iancenry@gmail.com>
2023-03-27 23:53:22 +0300

    space AboutUs.jsx

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -6,6 +6,7 @@ const AboutUs = () => (
     <div className='app__aboutus-overlay flex__center '>
       <img src={images.G} alt="big letter G" />
     </div>
+    
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
