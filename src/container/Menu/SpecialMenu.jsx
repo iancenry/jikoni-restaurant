1f638d0b8e3d9f98cd9ca4f0d0cb66e9cc4e9352
Ian Cenry <iancenry@gmail.com>
2023-03-29 00:22:00 +0300

    update SpecialMenu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -12,9 +12,30 @@ const SpecialMenu = () => (
 
     <div className="app__specialMenu-menu">
       <div className="app__specialMenu_wine flex__center">
-        <p>Wine & Beer</p>
-        <div className="app__specialMenu-menu_items"></div>
+        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <div className="app__specialMenu-menu_items">
+          {data.wines.map((wine, index) => (
+            <p>{wine.title}</p>
+          ))}
+        </div>
       </div>
+
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu img" />
+      </div>
+
+      <div className="app__specialMenu_cocktails flex__center">
+        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <div className="app__specialMenu-menu_items">
+          {data.cocktails.map((cocktail, index) => (
+            <p>{cocktail.title}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+
+    <div style={{marginTop: '15px'}}>
+      <button type='button' className='custom__button'>View More</button>
     </div>
   </div>
 );
