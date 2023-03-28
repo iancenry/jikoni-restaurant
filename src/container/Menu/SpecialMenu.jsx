52d3567e381200f3be4d9a67a1a0afbc7cc2921c
Ian Cenry <iancenry@gmail.com>
2023-03-28 23:45:34 +0300

    update SpecialMenu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import { SubHeading, MenuItem } from '../../components';
+import { images, data } from '../../constants';
 
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    <div className="app__specialMenu-title">
+      <SubHeading title='Menu That Fits Your Palette' />
+    </div>
   </div>
 );
 
