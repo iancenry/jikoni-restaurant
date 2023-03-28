8227767e3c5a1b39d86973bb7e063800f14e7d6c
Ian Cenry <iancenry@gmail.com>
2023-03-28 23:53:45 +0300

    update SpecialMenu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -7,6 +7,14 @@ const SpecialMenu = () => (
   <div className='app__specialMenu flex__center section__padding' id='menu'>
     <div className="app__specialMenu-title">
       <SubHeading title='Menu That Fits Your Palette' />
+      <h1 className='headtext__cormorant'>Todays' Special</h1>
+    </div>
+
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu_wine flex__center">
+        <p>Wine & Beer</p>
+        <div className="app__specialMenu-menu_items"></div>
+      </div>
     </div>
   </div>
 );
