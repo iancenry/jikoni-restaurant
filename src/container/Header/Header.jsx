56cd733410f1be4785659921c7e432e87233af88
Ian Cenry <iancenry@gmail.com>
2023-03-29 19:38:22 +0300

    update font size

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,13 +7,13 @@ import { SubHeading } from '../../components';
 const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home'>
     <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavor" />
-      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
-      <p className="p__opensans" style={{margin: '2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <SubHeading title="authentic kenyan cuisine" />
+      <h1 className='app__header-h1'>Kenyan Fine Dining Retold</h1>
+      <p className="p__opensans" style={{margin: '2rem 0'}}>Savor our exquisite foods sourced from Kenyan farms and freshly made with love and care and by our world class chefs.</p>
       <button type='button' className='custom__button'>Explore Menu</button>
     </div>
     <div className="app__wrapper_img">
-      <img src={images.welcome} alt="fine dining header image" />
+      <img src={images.welcome} alt="Kenya fine dining header image" />
     </div>
   </div>
 );
