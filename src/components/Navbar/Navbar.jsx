45fe21a5ab08330e22d7adc23b8177955349f5a7
Ian Cenry <iancenry@gmail.com>
2023-05-10 22:59:46 +0300

    update Footer components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import {GiHamburgerMenu} from 'react-icons/gi'
 import {MdOutlineRestaurantMenu} from 'react-icons/md'
 
@@ -13,12 +14,12 @@ const Navbar = () => {
     <div className="app__navbar-logo">
       <img src={images.jikoni} alt="app logo " />
     </div>
-    <ul className="app__navbar-links">
-      <li className="p__opensans"><a href="#home">Home</a></li>
-      <li className="p__opensans"><a href="#about">About</a></li>
-      <li className="p__opensans"><a href="#Menu">Menu</a></li>
-      <li className="p__opensans"><a href="#awards">Awards</a></li>
-      <li className="p__opensans"><a href="#contact">Contact</a></li>
+    <ul className="app__navbar-links">      
+      <li className="p__opensans"><Link to="/" >Home</Link></li>
+      <li className="p__opensans"><Link to='/about' >About</Link></li>
+      <li className="p__opensans"><Link to='/menu'>Menu</Link></li>
+      <li className="p__opensans"><Link to='/awards'>Awards</Link></li>
+      <li className="p__opensans"><Link to='/contact'>Contact</Link></li>
     </ul>
     <div className="app__navbar-login">
       <a href="#login" className='p__opensans'>Log In / Register</a>
@@ -30,12 +31,12 @@ const Navbar = () => {
       {toggleMenu && (
         <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
           <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
-          <ul className="app__navbar-smallscreen_links">
-            <li className="p__opensans"><a href="#home">Home</a></li>
-            <li className="p__opensans"><a href="#about">About</a></li>
-            <li className="p__opensans"><a href="#Menu">Menu</a></li>
-            <li className="p__opensans"><a href="#awards">Awards</a></li>
-            <li className="p__opensans"><a href="#contact">Contact</a></li>
+          <ul className="app__navbar-smallscreen_links"  onClick={() => setToggleMenu(false)}>
+            <li className="p__opensans" ><Link to="/" >Home</Link></li>
+            <li className="p__opensans"><Link to='/about' >About</Link></li>
+            <li className="p__opensans"><Link to='/menu'>Menu</Link></li>
+            <li className="p__opensans"><Link to='/awards'>Awards</Link></li>
+            <li className="p__opensans"><Link to='/contact'>Contact</Link></li>
           </ul>
         </div>
       )}
