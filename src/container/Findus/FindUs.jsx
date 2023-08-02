cd97671abcc708de81e2f7ac511684594b9c5e2d
Ian Cenry <iancenry@gmail.com>
2023-08-02 14:23:26 +0300

    feat: FindUs page

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,23 +1,71 @@
-import { SubHeading} from '../../components';
-import { images} from '../../constants';
+import { useState } from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './FindUs.css';
 
-const FindUs = () => (
-  <div className='app__bg app__wrapper section__padding' id='contact'>
-    <div className="app__wrapper_info">
-      <SubHeading title="Contact" />
-      <h1 className="headtext__cormorant" style={{marginBottom: '3rem'}}>Find Us</h1>
-      <div className="app__wrapper-content">
-        <p className="p__opensans">Westlands, Kenya</p>
-        <p className="p__cormorant" style={{color: 'DCCA87', margin: '2rem 0'}}>Opening Hours</p>
-        <p className="p__opensans">Mon - Fri: 10:00 am - 0200 am</p>
-        <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
+const FindUs = () => {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div className="app__bg app__wrapper section__padding" id="contact">
+      <div className="app__wrapper_info">
+        <SubHeading title="Contact" />
+        <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>
+          Find Us
+        </h1>
+        <div className="app__wrapper-content form-wrapper">
+          <form action="" className="contact-form">
+            <input
+              type="text"
+              value={formData.fullName}
+              placeholder="Full Name"
+              name="fullName"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              value={formData.email}
+              placeholder="Email Adress"
+              name="email"
+              onChange={handleChange}
+            />
+            <textarea
+              name="message"
+              rows="10"
+              placeholder="Message"
+              value={formData.message}
+              onChange={handleChange}
+            ></textarea>
+            <button
+              type="submit"
+              className="custom__button"
+              style={{ marginTop: '2rem' }}
+              onClick={handleSubmit}
+            >
+              Send
+            </button>
+          </form>
+        </div>
       </div>
-      <button type='button' className='custom__button' style={{marginTop: '2rem'}}>Visit Us</button>
+      <div className="app__wrapper_img">
+        <img src={images.findus} alt="find us" />
+      </div>
     </div>
-    <div className="app__wrapper_img">
-      <img src={images.findus} alt="find us" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default FindUs;
